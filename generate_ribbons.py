8a1fe3cc36899ea86963029389b494e8fe792dcc
mnygp <markus.markusnp@gmail.com>
2025-03-13 17:03:03 +0100

    Started on the function to generate the ribbons

--- generate_ribbons.py
+++ generate_ribbons.py
@@ -1,5 +1,11 @@
 from ase.build import graphene_nanoribbon
 from ase.visualize import view
+import warnings
 
 ribbon = graphene_nanoribbon(4, 7, type='armchair', saturated=True, vacuum=4)
 view(ribbon)
+
+
+def generate_ribbon(N, identifier, n, m):
+    if (identifier not in ['S', 'I']):
+        raise ValueError("Identifier must be 'S' or 'I'")
