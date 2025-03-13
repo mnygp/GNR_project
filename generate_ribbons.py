4833ec1e7f5db8621d3cc1ef53edb3edaf2ac9ee
mnygp <markus.markusnp@gmail.com>
2025-03-13 16:53:25 +0100

    Fixed import

--- generate_ribbons.py
+++ generate_ribbons.py
@@ -1,5 +1,5 @@
-from ase.build import graphene_ribbon
+from ase.build import graphene_nanoribbon
 from ase.visualize import view
 
-ribbon = graphene_ribbon(4, 7, type='armchair', saturated=True, vacuum=4)
+ribbon = graphene_nanoribbon(4, 7, type='armchair', saturated=True, vacuum=4)
 view(ribbon)
