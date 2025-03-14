391434a23129292b7e9d174b5062ac56c266c814
mnygp <markus.markusnp@gmail.com>
2025-03-14 11:18:46 +0100

    Added errors to catch invalid inputs

--- generate_ribbons.py
+++ generate_ribbons.py
@@ -5,22 +5,27 @@ from ase.visualize import view
 def generate_ribbon(N, identifier, n, m, vac=5):
     if (identifier not in ['S', 'I']):
         raise ValueError("Identifier must be 'S' or 'I'")
-
-    if (type(N) is not int):
+    elif (N % 1 != 0):
         raise TypeError("N must be an integer")
-
-    if (m < 2):
+    elif (m < 2):
         raise ValueError("m must be greater than 1")
 
     if (identifier == 'S'):
-        length = 3
+        if ((N % 2 == 0) and m % 1 == 0):
+            raise ValueError("For N even m must be a half integer")
+        elif ((N % 2 == 1) and m % 1 != 0):
+            raise ValueError("For N odd m must be an integer")
+        elif (m % 1 != 0 or m % 1 != 0.5):
+            raise ValueError("m must be integer or half integer")
+
+        length = 2*(2*n+1)
+        if (N % 2 == 0):
+            length += m-1.5
         ribbon = graphene_nanoribbon(N/2.0, length,
                                      type='armchair', vacuum=vac)
         print('S')
 
     if (identifier == 'I'):
-        if (m % 2 == 0):
-            raise ValueError("m must be odd for I type ribbons")
 
         length = (2*n + 1) + (m - 3)
         ribbon = graphene_nanoribbon(N/2.0, length,
@@ -30,5 +35,5 @@ def generate_ribbon(N, identifier, n, m, vac=5):
     return ribbon
 
 
-ribbon = generate_ribbon(7, 'I', 1, 5)
+ribbon = generate_ribbon(7, 'I', 1, 3)
 view(ribbon)
