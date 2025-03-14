a85771811e7ad13ae4b4bac68f7d60e5d75b5ea3
mnygp <markus.markusnp@gmail.com>
2025-03-14 10:41:47 +0100

    Calculate length of I type unit cell

--- generate_ribbons.py
+++ generate_ribbons.py
@@ -1,11 +1,34 @@
 from ase.build import graphene_nanoribbon
 from ase.visualize import view
-import warnings
-
-ribbon = graphene_nanoribbon(4, 7, type='armchair', saturated=True, vacuum=4)
-view(ribbon)
 
 
-def generate_ribbon(N, identifier, n, m):
+def generate_ribbon(N, identifier, n, m, vac=5):
     if (identifier not in ['S', 'I']):
         raise ValueError("Identifier must be 'S' or 'I'")
+
+    if (type(N) is not int):
+        raise TypeError("N must be an integer")
+
+    if (m < 2):
+        raise ValueError("m must be greater than 1")
+
+    if (identifier == 'S'):
+        length = 3
+        ribbon = graphene_nanoribbon(N/2.0, length,
+                                     type='armchair', vacuum=vac)
+        print('S')
+
+    if (identifier == 'I'):
+        if (m % 2 == 0):
+            raise ValueError("m must be odd for I type ribbons")
+
+        length = (2*n + 1) + (m - 3)
+        ribbon = graphene_nanoribbon(N/2.0, length,
+                                     type='armchair', vacuum=vac)
+        print('I')
+
+    return ribbon
+
+
+ribbon = generate_ribbon(7, 'I', 1, 5)
+view(ribbon)
