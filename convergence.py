78eef78f6c66030903f40e665dfca217cf23ecee
mnygp <markus.markusnp@gmail.com>
2025-03-24 17:29:00 +0100

    Added plotting and printing

--- convergence.py
+++ convergence.py
@@ -1,6 +1,10 @@
 from generate_ribbons import generate_ribbon, saturate_edges
+
 from gpaw import GPAW, PW
+from ase.parallel import parprint
+
 import numpy as np
+from matplotlib import pyplot as plt
 
 folder = "convergence_files/"
 
@@ -24,7 +28,7 @@ for k in range(1, 11):
     energy_k = np.append(energy_k, energy)
 
 
-for cut in range(300, 601, 50):
+for cut in range(200, 601, 40):
     calc = GPAW(mode=PW(cut),
                 xc='PBE',
                 kpts=(1, 1, 8),
@@ -46,3 +50,33 @@ for vac in range(1, 11):
 
     energy = ribbon_vac.get_potential_energy()
     energy_vac = np.append(energy_k, energy)
+
+parprint("Convergence tests:")
+parprint("k-points 1 to 10")
+parprint(energy_k)
+parprint("Plane-wave cutoff 200 to 600 in steps of 40")
+parprint(energy_cut)
+parprint("Vacuum 1Å to 10Å")
+parprint(energy_vac)
+
+
+fig_k, ax_k = plt.subplots()
+ax_k.plot(range(1, 11), energy_k, 'o-')
+ax_k.set_xlabel('k-points')
+ax_k.set_ylabel('Energy (eV)')
+ax_k.set_title('Convergence with k-points')
+fig_k.savefig(folder + 'AGNR_S_3_1_k.png')
+
+fig_cut, ax_cut = plt.subplots()
+ax_cut.plot(range(200, 601, 40), energy_cut, 'o-')
+ax_cut.set_xlabel('Plane-wave cutoff (eV)')
+ax_cut.set_ylabel('Energy (eV)')
+ax_cut.set_title('Convergence with plane-wave cutoff')
+fig_cut.savefig(folder + 'AGNR_S_3_1_cut.png')
+
+fig_vac, ax_vac = plt.subplots()
+ax_vac.plot(range(1, 11), energy_vac, 'o-')
+ax_vac.set_xlabel('Vacuum (Å)')
+ax_vac.set_ylabel('Energy (eV)')
+ax_vac.set_title('Convergence with vacuum')
+fig_vac.savefig(folder + 'AGNR_S_3_1_vac.png')
