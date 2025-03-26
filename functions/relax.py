e1a909ba2c27bbcd1247783ad493b5050eae7812
mnygp <markus.markusnp@gmail.com>
2025-03-26 16:55:20 +0100

    Added converged parameters as default values for relaxation functions

--- functions/relax.py
+++ functions/relax.py
@@ -3,7 +3,7 @@ from ase.filters import UnitCellFilter
 from gpaw import GPAW, PW
 
 
-def relax_PW(structure, PW_cut, k_pts, filename, func='PBE'):
+def relax_PW(structure, filename, PW_cut=500, k_pts=6, func='PBE'):
     calc = GPAW(mode=PW(PW_cut),
                 xc=func,
                 kpts=(1, 1, k_pts),
@@ -12,12 +12,12 @@ def relax_PW(structure, PW_cut, k_pts, filename, func='PBE'):
     structure.set_calculator(calc)
     uf = UnitCellFilter(structure)
     relax = BFGS(uf)
-    relax.run(fmax=0.05)
+    relax.run(fmax=0.01)
 
     return structure
 
 
-def relax_LCAO(structure, k_pts, filename, func='PBE', basis='dzp'):
+def relax_LCAO(structure, filename, k_pts=6, func='PBE', basis='dzp'):
     calc = GPAW(mode='lcao',
                 basis=basis,
                 xc=func,
@@ -27,6 +27,6 @@ def relax_LCAO(structure, k_pts, filename, func='PBE', basis='dzp'):
     structure.set_calculator(calc)
     uf = UnitCellFilter(structure)
     relax = BFGS(uf)
-    relax.run(fmax=0.05)
+    relax.run(fmax=0.01)
 
     return structure
