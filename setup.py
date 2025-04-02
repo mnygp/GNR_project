bc35ee1614c8ffa8d526884481b7ee962ac1581c
mnygp <markus.markusnp@gmail.com>
2025-04-02 09:21:52 +0200

    Made the functions folder into a proper package

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="GNR project",  # Replace with your project name
+    version="0.1",
+    packages=find_packages(),  # Automatically finds all packages (folders with __init__.py)
+)
