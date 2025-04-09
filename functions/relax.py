01c3c6b0a7240944cb4aada145c258e75d421f54
mnygp <markus.markusnp@gmail.com>
2025-04-09 15:32:34 +0200

    Added the option to write trajectory files during relaxation

--- functions/relax.py
+++ functions/relax.py
@@ -1,11 +1,14 @@
 from ase.optimize.bfgs import BFGS
 from ase.filters import UnitCellFilter
-from gpaw import GPAW, PW
+from ase.io import Trajectory
 from ase import Atoms
+
+from gpaw import GPAW, PW
 
 
 def relax(structure: Atoms, filename: str, PW_toggle: bool,
-          params: dict[str, str | float], k: int = 6) -> Atoms:
+          params: dict[str, str | float], k: int = 6,
+          traj_name: str | None = None) -> Atoms:
     func = params['func']
     if PW_toggle:
         PW_cut = params['PW_cut']
@@ -23,16 +26,28 @@ def relax(structure: Atoms, filename: str, PW_toggle: bool,
 
     structure.calc = calc
     uf = UnitCellFilter(structure)
-    relax = BFGS(uf)
+    if traj_name is not None:
+        traject = Trajectory(traj_name+'.traj', 'w', uf)
+        relax = BFGS(uf, trajectory=traject)
+    else:
+        relax = BFGS(uf)
     relax.run(fmax=0.01)
 
     return structure
 
 
 def multi_step_relax(structure: Atoms, filename: str, PW_toggle: bool,
-                     params: dict[str], k_arr: list[int]) -> Atoms:
+                     params: dict[str], k_arr: list[int],
+                     traj_name: str | None = None) -> Atoms:
 
-    for k in k_arr:
-        structure = relax(structure, filename+f"_k_{k}", PW_toggle, params, k)
+    if traj_name is not None:
+        for k in k_arr:
+            traj_file_name = traj_name + f'_k_{k}'
+            structure = relax(structure, traj_name, PW_toggle, params, k,
+                              traj_name=traj_file_name)
+    else:
+        for k in k_arr:
+            structure = relax(structure, filename+f'_k_{k}',
+                              PW_toggle, params, k)
 
     return structure
