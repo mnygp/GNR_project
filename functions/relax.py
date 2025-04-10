6f7985e9cd2d01421addb25280e5b3984d5e7034
mnygp <markus.markusnp@gmail.com>
2025-04-10 15:12:42 +0200

    Added some explaining text to the module and expanded the functionality of the multi_step_relax function

--- functions/relax.py
+++ functions/relax.py
@@ -5,10 +5,28 @@ from ase import Atoms
 
 from gpaw import GPAW, PW
 
+"""
+This module contains functions to relax a structure using GPAW.
+It provides functions to perform a single relaxation step or multiple
+relaxation steps with different k-point meshes or basis sets.
+The `relax` function performs a single relaxation step, while the
+`multi_step_relax` function allows for multiple relaxation steps
+with different parameters.
+The `trajectory_file_name` function generates a filename for the
+trajectory based on the provided parameters.
+
+The parameters dictionary for the relaxation can include:
+- `func`: The exchange-correlation functional to use (e.g., 'PBE').
+- `basis`: The basis set to use (e.g., 'dzp') either as a string or
+           a list of strings.
+- `PW_cut`: The plane-wave cutoff energy (in eV) for the GPAW calculator.
+"""
+
 
 def relax(structure: Atoms, filename: str, PW_toggle: bool,
           params: dict[str, str | float], k: int = 6,
           traj_name: str | None = None) -> Atoms:
+
     func = params['func']
     if PW_toggle:
         PW_cut = params['PW_cut']
@@ -37,17 +55,42 @@ def relax(structure: Atoms, filename: str, PW_toggle: bool,
 
 
 def multi_step_relax(structure: Atoms, filename: str, PW_toggle: bool,
-                     params: dict[str], k_arr: list[int],
-                     traj_name: str | None = None) -> Atoms:
+                     params: dict[str, str | float | list[str]],
+                     k_arr: list[int], traj_name: str | None = None) -> Atoms:
 
-    if traj_name is not None:
-        for k in k_arr:
-            traj_file_name = traj_name + f'_k_{k}'
-            structure = relax(structure, traj_name, PW_toggle, params, k,
-                              traj_name=traj_file_name)
-    else:
-        for k in k_arr:
-            structure = relax(structure, filename+f'_k_{k}',
-                              PW_toggle, params, k)
+    if type(params['basis']) is list:
+        if len(params['basis']) > 1 and len(params['basis']) != len(k_arr):
+            raise ValueError("If multiple basis sets are provided," +
+                             " the number of basis sets must match" +
+                             " the number of k-points.")
+        basis_list = params['basis']
+
+    for i, k in enumerate(k_arr):
+        if PW_toggle:
+            traj_file_name = trajectory_file_name(traj_name, k,
+                                                  PW_cut=params['PW_cut'])
+        else:
+            traj_file_name = trajectory_file_name(traj_name, k,
+                                                  basis=params['basis'])
+
+        if type(params['basis']) is list:
+            params['basis'] = basis_list[i]
+
+        structure = relax(structure, filename, PW_toggle, params, k,
+                          traj_name=traj_file_name)
 
     return structure
+
+
+def trajectory_file_name(traj_name: str, k: int, basis: str | None = None,
+                         PW_cut: float | None = None) -> str:
+
+    if traj_name is not None:
+        if basis is None and PW_cut is not None:
+            traj_file_name = traj_name + f'_k_{k}_{PW_cut}'
+        elif basis is not None and PW_cut is None:
+            traj_file_name = traj_name + f'_k_{k}_{basis}'
+    else:
+        traj_file_name = None
+
+    return traj_file_name
