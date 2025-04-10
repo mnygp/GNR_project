b2834ab819004fc77e1354f3436f01ba7a29ddc8
mnygp <markus.markusnp@gmail.com>
2025-04-10 16:37:53 +0200

    Switched from setup.py to pyproject.toml

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="GNR project",  # Replace with your project name
-    version="0.1",
-    # Automatically finds all packages (folders with __init__.py)
-    packages=find_packages(),
-)
