3e50b0924f9af58f45c12990637ea571871e8705
mnygp <markus.markusnp@gmail.com>
2025-04-22 10:46:04 +0200

    Minor fixes

--- backbone_structure/plotting.py
+++ backbone_structure/plotting.py
@@ -13,7 +13,7 @@ post = data[order, 3]
 
 plt.plot(width, pre, '-o', label="pre relaxation")
 plt.plot(width, post, '-o', label="post relaxation")
-plt.xlim(1,21)
+plt.xlim(1, 21)
 plt.xticks(np.linspace(2, 20, 10, dtype=int))
 plt.xlabel("Width")
 plt.ylabel("Gap energy [eV]")
