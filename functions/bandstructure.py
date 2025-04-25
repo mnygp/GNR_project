b22d17b35f6769bd7bf82e85158fe4178f6e5af7
mnygp <markus.markusnp@gmail.com>
2025-04-25 13:09:20 +0200

    Now ldos calculations actually run

--- functions/bandstructure.py
+++ functions/bandstructure.py
@@ -26,11 +26,14 @@ def get_gap(ribbon: Atoms, params: RelaxParams, k: int = 6,
     return gap
 
 
-def LDOS(ribbon: Atoms, calc_params: RelaxParams, site_index: int, k: int = 6):
-    PW_cut = calc_params.get('PW_cut')
-    assert PW_cut is not None, "PW_cut is required"
+def LDOS(ribbon: Atoms, calc_params: RelaxParams, site_index: int,
+         k: int = 6, npoints: int = 301):
+
+    basis = calc_params.get('basis')
+    assert basis is not None, "lcao mode is required"
     func = calc_params['func']
-    calc = GPAW(mode=PW(PW_cut),
+    calc = GPAW(mode='lcao',
+                basis=basis,
                 xc=func,
                 kpts={'size': (1, 1, k)},
                 occupations=FermiDirac(0.01),
@@ -40,6 +43,8 @@ def LDOS(ribbon: Atoms, calc_params: RelaxParams, site_index: int, k: int = 6):
 
     ribbon.get_potential_energy()
 
-    energies, ldos = calc.get_wigner_seitz_ldos(site_index)
+    energies, ldos = calc.get_lcao_dos(atom_indices=site_index,
+                                       npts=npoints,
+                                       width=0.1)
 
     return energies, ldos
